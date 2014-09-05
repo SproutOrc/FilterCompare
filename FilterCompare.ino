8de7ef6ae028621bed3a685d163ec7b7251b04dd
SproutOrc <orcatropos@gmail.com>
2014-09-05 11:33:05 +0800

    3

--- FilterCompare.ino
+++ FilterCompare.ino
@@ -11,13 +11,8 @@
 
 #define ACCEL 0x3B
 #define GYRO  0x43
-double KP = 40;
-double KI = 38;
-double KD = 0.9;
-double SET_POINT = 9;
 #define SAMPLE_TIME 20
 #define SPEED 210.0
-
 
 uint8_t ADR = 0x68;
 
@@ -25,101 +20,84 @@ uint8_t MAG_ADR = 0x0c;
 
 uint8_t buffer[6]; 
 
-// Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-
-// Specify the links and initial tuning parameters
-PID MotorPID(&Input, &Output, &Setpoint,KP,KI,KD, DIRECT);
-
 SelfHardwareMake motion;
 
-Timer t;//时间类
-
-float timeChange=SAMPLE_TIME;//滤波法采样时间间隔毫秒
-float dt=timeChange*0.001;//注意：dt的取值为滤波器采样时间
+//时间类
+Timer t;
+//滤波法采样时间间隔毫秒
+float timeChange=SAMPLE_TIME;
+//注意：dt的取值为滤波器采样时间
+float dt=timeChange*0.001;
 // 陀螺仪
-float angleAx,gyroGy;//计算后的角度（与x轴夹角）和角速度
+// 计算后的角度（与x轴夹角）和角速度
+float angleAx,gyroGy;
+// 陀螺仪原始数据 3个加速度+3个角速度
+int16_t ax, ay, az, gx, gy, gz;
+/////////////////////////////////////////////////
+///////////////PID///////////////////////////////
+unsigned long lastTime;
+double Input, Output, Setpoint;
+double errSum, lastErr;
+int SampleTime = 1000; //1 sec
 
-int16_t ax, ay, az, gx, gy, gz;//陀螺仪原始数据 3个加速度+3个角速度
+double kp = 40;
+double ki = 38;
+double kd = 0.9;
 
-//一阶滤波
-float K1 =0.05; // 对加速度计取值的权重
-//float dt=20*0.001;//注意：dt的取值为滤波器采样时间
-float angle1;//一阶滤波角度输出
-//二阶滤波
-float K2 =0.2; // 对加速度计取值的权重
-float x1,x2,y1;//运算中间变量
-//float dt=20*0.001;//注意：dt的取值为滤波器采样时间
-float angle2;//er阶滤波角度输出
+double KP = 40;
+double KI = 38;
+double KD = 0.9;
+double SET_POINT = 9;
 
-//卡尔曼滤波参数与函数
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+////////////////卡尔曼滤波参数与函数/////////////
 float angle, angle_dot;//角度和角速度
 float angle_0, angle_dot_0;//采集来的角度和角速度
-//float dt=20*0.001;//注意：dt的取值为kalman滤波器采样时间
-//一下为运算中间变量
+// 一下为运算中间变量
 float P[2][2] = {{ 1, 0 },
               { 0, 1 }};
 float Pdot[4] ={ 0,0,0,0};
-float Q_angle=0.001, Q_gyro=0.003; //角度数据置信度,角速度数据置信度
+ //角度数据置信度,角速度数据置信度
+float Q_angle=0.001, Q_gyro=0.003;
 float R_angle=0.5 ,C_0 = 1; 
 float q_bias, angle_err, PCt_0, PCt_1, E, K_0, K_1, t_0, t_1;
-
+/////////////////////////////////////////////////
+///
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     Wire.begin();
-    TWBR = 2; // 24: 400kHz I2C clock (200kHz if CPU is 8MHz) //2014.01.10変えてみた．
-    //TWBR = 12; // 12;400kHz I2C clock (400kHz if CPU is 16MHz)
+    // 24: 400kHz I2C clock (200kHz if CPU is 8MHz)
+    TWBR = 2;
+    //TWBR = 12; 
+    // 12;400kHz I2C clock (400kHz if CPU is 16MHz)
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
     Fastwire::setup(400, true);
     #endif
-
     Serial1.begin(57600);
-
-    // Serial.println("Initializing I2C devices...");
-
     uint8_t who_am_i;
     I2Cdev::readByte(ADR, 0x75, &who_am_i);
-    // if(who_am_i == 0x71){
-    //   Serial.println("Successfully connected to MPU9250");
-    // }
-    // else{
-    //   Serial.println("Failed to Connect to MPU9250");
-    // }
-
     I2Cdev::writeByte(ADR, 0x1b, 0x00);
     delay(1);
-
     I2Cdev::writeByte(ADR, 0x1c, 0x00);
     delay(1);
-
     I2Cdev::writeByte(ADR, 0x6b, 0x00);
     delay(1);
-
     I2Cdev::writeByte(ADR, 0x37, 0x02);
-
     I2Cdev::readByte(MAG_ADR,0x00,&who_am_i);
-    if(who_am_i == 0x48){
-    // Serial.println("Successfully connected to COMPASS(AK8963)");
-    }
-    else{
-    // Serial.println("Failed to Connect to COMPASS(AK8963)");
-    }
     delay(5);
-
     I2Cdev::writeByte(MAG_ADR, 0x0a, 0x00);
     delay(10);
-
     I2Cdev::writeByte(MAG_ADR, 0x0a, 0x16);
     delay(10); 
+    //本语句执行以后timeChange毫秒执行回调函数getangle
+    int tickEvent1=t.every(timeChange, getangle);
+    //本语句执行以后50毫秒执行回调函数printout，串口输出
+    int tickEvent2=t.every(50, printout);
 
-    int tickEvent1=t.every(timeChange, getangle);//本语句执行以后timeChange毫秒执行回调函数getangle
-
-    int tickEvent2=t.every(50, printout) ;//本语句执行以后50毫秒执行回调函数printout，串口输出
-    MotorPID.SetOutputLimits(-SPEED, SPEED);
-    MotorPID.SetMode(AUTOMATIC);
-    MotorPID.SetSampleTime(SAMPLE_TIME);
-    MotorPID.SetTunings(KP, KI, KD);
-    Setpoint = SET_POINT;
+    SetSampleTime(SAMPLE_TIME);
+    SetTunings(KP, KI, KD);
 }   
 
 void loop() {
@@ -134,7 +112,7 @@ void printout()
         KD = (double)Serial1.read();
         Setpoint = (double)Serial1.read();
 
-        MotorPID.SetTunings(KP, KI, KD);
+        SetTunings(KP, KI, KD);
         Serial1.print("KP = ");
         Serial1.print(KP);
         Serial1.print("\tKI = ");
@@ -175,7 +153,7 @@ void getangle()
         return;
     }
     Input = angle;
-    MotorPID.Compute();
+    Compute();
 
 
     if (Output > 0) {
@@ -213,4 +191,44 @@ void Kalman_Filter(double angle_m,double gyro_m)
     angle += K_0 * angle_err; //最优角度
     q_bias += K_1 * angle_err;
     angle_dot = gyro_m-q_bias;//最优角速度
+}
+
+void Compute()
+{
+   unsigned long now = millis();
+   int timeChange = (now - lastTime);
+   if(timeChange>=SampleTime)
+   {
+      /*Compute all the working error variables*/
+      double error = Setpoint - Input;
+      errSum += error;
+      double dErr = (error - lastErr);
+ 
+      /*Compute PID Output*/
+      Output = kp * error + ki * errSum + kd * dErr;
+ 
+      /*Remember some variables for next time*/
+      lastErr = error;
+      lastTime = now;
+   }
+}
+ 
+void SetTunings(double Kp, double Ki, double Kd)
+{
+  double SampleTimeInSec = ((double)SampleTime)/1000;
+   kp = Kp;
+   ki = Ki * SampleTimeInSec;
+   kd = Kd / SampleTimeInSec;
+}
+ 
+void SetSampleTime(int NewSampleTime)
+{
+   if (NewSampleTime > 0)
+   {
+      double ratio  = (double)NewSampleTime
+                      / (double)SampleTime;
+      ki *= ratio;
+      kd /= ratio;
+      SampleTime = (unsigned long)NewSampleTime;
+   }
 }
